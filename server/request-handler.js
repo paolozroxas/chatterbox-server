fa7eb8186dafd537a58a3dd431b56fa6835f423a
paolozroxas <paolo.z.roxas@gmail.com>
2018-02-05 16:45:32 -0800

    working request for index.html

--- server/request-handler.js
+++ server/request-handler.js
@@ -1,32 +1,11 @@
-/*************************************************************
+const http = require('http');
+const url = require('url');
+const querystring = require('querystring');
+const path = require('path');
+const fs = require('fs');
 
-You should implement your request handler function in this file.
-
-requestHandler is already getting passed to http.createServer()
-in basic-server.js, but it won't work as is.
-
-You'll have to figure out a way to export this function from
-this file and include it in basic-server.js so that it actually works.
-
-*Hint* Check out the node module documentation at http://nodejs.org/api/modules.html.
-
-**************************************************************/
-var http = require('http');
-var url = require('url');
-var querystring = require('querystring');
-
-
-//modules to be exported.
 var exports = module.exports = {};
-// These headers will allow Cross-Origin Resource Sharing (CORS).
-// This code allows this server to talk to websites that
-// are on different domains, for instance, your chat client.
-//
-// Your chat client is running from a url like file://your/chat/client/index.html,
-// which is considered a different domain.
-//
-// Another way to get around this restriction is to serve you chat
-// client from this domain by setting up static file serving.
+
 var defaultCorsHeaders = {
   'access-control-allow-origin': '*',
   'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
@@ -34,52 +13,67 @@ var defaultCorsHeaders = {
   'access-control-max-age': 10 // Seconds.
 };
 
-//exports.server = http.createServer().listen(3000);
-
 exports.requestHandler = function(request, response) {
-  // Request and Response come from node's http module.
-  //
-  // They include information about both the incoming request, such as
-  // headers and URL, and about the outgoing response, such as its status
-  // and content.
-  //
-  // Documentation for both request and response can be found in the HTTP section at
-  // http://nodejs.org/documentation/api/
 
-  // See the note below about CORS headers.
   var headers = defaultCorsHeaders;
-
-  // Tell the client we are sending them plain text.
-  //
-  // You will need to change this if you are sending something
-  // other than plain text, like JSON or HTML.
   headers['Content-Type'] = 'text/plain';
-
-  // The outgoing status.
   var statusCode = 200;
 
-  // Do some basic logging.
-  //
-  // Adding more logging to your server can be an easy way to get passive
-  // debugging help, but you should always be careful about leaving stray
-  // console.logs in your code.
   console.log('Serving request type ' + request.method + ' for url ' + request.url);
   
-  if (request.method === 'POST') {
-    var body = '';
+  var parsedUrl = url.parse(request.url);
+  
+  
+  // if (request.method === 'POST') {
+  //   var body = '';
+  //   request.on('data', function() {
+  //     body += data;
+  //   });
+
+  //   request.on('end', function() {
+  //     var post = querystring.parse(body);
+  //   });
+  // }
+  var pathname = `client${parsedUrl.pathname}`;
+  
+  if (pathname === 'client/chatterbox') {
+    pathname = 'client/';
   }
   
-  request.on('data', function() {
-    body += data;
+  console.log('the pathname is ', pathname);
+
+  //check if file exists
+  fs.exists(pathname, function (exist) {
+    if (!exist) {
+      response.statusCode = 404;
+      response.end(`File ${pathname} not found!`);
+      return;
+    } else {
+      console.log('the file exists!', pathname);
+    }
   });
 
-  request.on('end', function() {
-    var post = querystring.parse(body);
-    console.log(post);
-    response.writeHead(statusCode, headers);
-    response.end('Hello, World!');
-  });
-
+  if (pathname === 'client/') {
+    
+    //check if is a directory
+    if (fs.statSync(pathname).isDirectory()) {
+      pathname = 'client/index.html';
+    }
+    
+    //read file
+    fs.readFile(pathname, function (err, data) {
+      if (err) {
+        console.log('error getting ', pathname);
+        response.statusCode = 500;
+        response.end('error getting ', pathname);
+      } else {
+        console.log('success getting ', pathname);
+        response.setHeader('Content-type', 'text/html');
+        response.end(data);
+      }
+    });
+    
+  }
 };
 
-//exports.server.on('request', exports.requestHandler);
+
